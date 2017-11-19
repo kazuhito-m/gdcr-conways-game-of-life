c47d634f50b079a56bd4c3058fe90b573c33f3d4
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-11-19 20:03:40 +0900

    実装。座標クラス。Green.

--- src/main/java/com/github/kazuhito_m/gdcrconwaysgameoflife/domain/model/Position.java
+++ src/main/java/com/github/kazuhito_m/gdcrconwaysgameoflife/domain/model/Position.java
@@ -5,6 +5,17 @@ public class Position {
     final int row;
     final int column;
 
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Position)) return false;
+        return other.hashCode() == this.hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return row * 100000 + column;
+    }
+
     public Position(int row, int column) {
         this.row = row;
         this.column = column;
