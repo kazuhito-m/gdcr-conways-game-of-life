b85c6e7ad096e117319c5c296d007d4bc1216930
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-11-20 00:13:59 +0900

    リファクタリング。cellのリストはファーストクラスコレクションとして切り出し。

---
+++ src/main/java/com/github/kazuhito_m/gdcrconwaysgameoflife/domain/model/Cells.java
@@ -0,0 +1,66 @@
+package com.github.kazuhito_m.gdcrconwaysgameoflife.domain.model;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toMap;
+
+public class Cells {
+
+    final Map<Position, Cell> cells;
+
+    public Cell of(Position position) {
+        if (!cells.containsKey(position)) return new Cell(false);
+        return cells.get(position);
+    }
+
+    public Set<Position> positions() {
+        return cells.keySet();
+    }
+
+    public Cells nextGeneration() {
+        Map<Position, Cell> nextGenerations = positions().stream()
+                .collect(toMap(
+                        position -> position,
+                        position -> nextGenerationCell(position)
+                ));
+        return new Cells(nextGenerations);
+    }
+
+    private Cell nextGenerationCell(Position position) {
+        int neighborhoodCount = countNeighborhood(position);
+        Cell currentCell = cells.get(position);
+        return currentCell.nextGeneration(neighborhoodCount);
+    }
+
+    int countNeighborhood(Position position) {
+        int count = 0;
+        int currentRow = position.row();
+        int currentColumn = position.column();
+        for (int row = currentRow - 1; row < currentRow + 1; row++) {
+            for (int column = currentColumn - 1; column < currentColumn + 1; column++) {
+                Position nowPosition = new Position(row, column);
+                if (position.equals(nowPosition)) continue;
+                if (of(nowPosition).alive()) count++;
+            }
+        }
+        return count;
+    }
+
+    public static Cells initialize(WorldCondition condition) {
+        Map<Position, Cell> newCells = new HashMap<>();
+        for (int row = 0; row < condition.rowCount(); row++) {
+            for (int column = 0; column < condition.columnCount(); column++) {
+                newCells.put(new Position(row, column), Cell.randomInitialize());
+            }
+        }
+        return new Cells(Collections.unmodifiableMap(newCells));
+    }
+
+    public Cells(Map<Position, Cell> cells) {
+        this.cells = cells;
+    }
+
+}
--- src/main/java/com/github/kazuhito_m/gdcrconwaysgameoflife/domain/model/World.java
+++ src/main/java/com/github/kazuhito_m/gdcrconwaysgameoflife/domain/model/World.java
@@ -1,76 +1,35 @@
 package com.github.kazuhito_m.gdcrconwaysgameoflife.domain.model;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.util.stream.Collectors.toMap;
-
 public class World {
 
     final WorldCondition condition;
-    final Map<Position, Cell> cells;
+    final Cells cells;
 
     public int[][] matrix() {
         int[][] result = condition.blankMatrix();
-        cells.keySet().stream()
+        cells.positions().stream()
                 .forEach(position -> setAlive(result, position));
         return result;
     }
 
     private void setAlive(int[][] matrix, Position position) {
-        Cell cell = cells.get(position);
+        Cell cell = cells.of(position);
         int aliveValue = cell.alive() ? 1 : 0;
         matrix[position.row()][position.column()] = aliveValue;
     }
 
     public World nextGeneration() {
-        Map<Position, Cell> nextGenerationCells = cells.keySet().stream()
-                .collect(toMap(
-                        position -> position,
-                        position -> nextGenerationCell(position)
-                ));
-        return new World(condition, nextGenerationCells);
+        return new World(condition, cells.nextGeneration());
     }
 
-    private Cell nextGenerationCell(Position position) {
-        int neighborhoodCount = countNeighborhood(position);
-        Cell currentCell = cells.get(position);
-        return currentCell.nextGeneration(neighborhoodCount);
-    }
 
-    private int countNeighborhood(Position position) {
-        int count = 0;
-        int currentRow = position.row();
-        int currentColumn = position.column();
-        for (int row = currentRow - 1; row < currentRow + 1; row++) {
-            for (int column = currentColumn - 1; column < currentColumn + 1; column++) {
-                Position nowPosition = new Position(row, column);
-                if (position.equals(nowPosition)) continue;
-                if (cells.containsKey(nowPosition) && cells.get(nowPosition).alive()) count++;
-            }
-        }
-        return count;
-    }
-
-    private static Map<Position, Cell> initialCells(WorldCondition condition) {
-        Map<Position, Cell> cells = new HashMap<>();
-        for (int row = 0; row < condition.rowCount(); row++) {
-            for (int column = 0; column < condition.columnCount(); column++) {
-                cells.put(new Position(row, column), Cell.randomInitialize());
-            }
-        }
-        return Collections.unmodifiableMap(cells);
-    }
-
-    World(WorldCondition condition, Map<Position, Cell> cells) {
+    World(WorldCondition condition, Cells cells) {
         this.condition = condition;
         this.cells = cells;
     }
 
     public World(WorldCondition condition) {
-        this(condition, initialCells(condition));
+        this(condition, Cells.initialize(condition));
     }
-
 
 }
