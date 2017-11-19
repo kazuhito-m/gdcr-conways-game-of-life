855c463235454211ed7fdff693292400f2d361e7
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-11-19 22:43:16 +0900

    テスト実装。次の世代の世界のテスト。

--- src/test/java/com/github/kazuhito_m/gdcrconwaysgameoflife/domain/model/WorldTest.java
+++ src/test/java/com/github/kazuhito_m/gdcrconwaysgameoflife/domain/model/WorldTest.java
@@ -18,6 +18,18 @@ public class WorldTest {
         assertThat(aliveCount(matrix)).isBetween(4000, 6000);
     }
 
+    @Test
+    public void 二世代目の世界で一つ以上のセルが生存している() {
+        WorldCondition condition = new WorldCondition(100, 100);
+        World sut = new World(condition);
+
+        World worldOfNextGeneration = sut.next();
+
+        int[][] matrix = worldOfNextGeneration.matrix();
+
+        assertThat(aliveCount(matrix)).isGreaterThanOrEqualTo(1);
+    }
+
     private int aliveCount(int[][] matrix) {
         return (int) Arrays.stream(matrix)
                 .flatMapToInt(Arrays::stream)
